5c170a875d9780bca79266aa622b9c51ef1e37d2
Moaaz Khan <moaazkhan33@gmail.com>
2017-01-06 02:06:53 +0500

    Bootstrap theme changed and New article option added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  get "/", to: "articles#index"
+
+ get "/articles/new", to: "articles#new", as: "articlenew"
+ post "articles/new", to: "articles#create"
  get "/articles/:id", to: "articles#show", as: "article"
 end
