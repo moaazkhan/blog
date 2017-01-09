3087822e77b19f9c7fe915ad17ecc03dfcac1129
Moaaz Khan <moaazkhan33@gmail.com>
2017-01-10 03:18:28 +0500

    Comments section added to the article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,8 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+    @comments = @article.comments
+    @new_comment = Comment.new
   end
 
   def new
@@ -12,13 +14,34 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    Article.create(article_params)
+    Article.create!(article_params)
     redirect_to action: :index
+  end
+
+  def create_comment
+    # Find the article using the id param passed in route
+    @article = Article.find(params[:id])
+
+    # Initialize a new Comment with content params
+    @comment = Comment.new(comment_params)
+
+    # Assign the comment to our current article
+    @comment.article_id = @article.id
+
+    # Finally save the comment
+    @comment.save
+
+    # Redirect to the current article page (/articles/current-id)
+    redirect_to action: :show
   end
 
   private
 
   def article_params
     params.require(:article).permit(:title, :content)
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
- get "/", to: "articles#index"
-
- get "/articles/new", to: "articles#new", as: "articlenew"
- post "articles/new", to: "articles#create"
- get "/articles/:id", to: "articles#show", as: "article"
+  get "/", to: "articles#index"
+  get "/articles/new", to: "articles#new", as: "articlenew"
+  post "/articles/new", to: "articles#create"
+  get "/articles/:id", to: "articles#show", as: "article"
+  post "/articles/:id/comment", to: "articles#create_comment", as: "comment"
 end
